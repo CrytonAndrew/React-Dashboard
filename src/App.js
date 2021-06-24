a623846b43eadd5676b0c51ce93504e9d13f5994
Cryton Andrew <crytonadrew21@gmail.com>
2021-06-24 02:36:59 +0200

    Remove area chart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import PopChart from './components/PopChart';
 import LineChart from './components/LineChart';
+import DonutChart from './components/DonutChart';
+
 function App() {
   return (
     <div className="App">
       <PopChart />
       <LineChart />
+      <DonutChart />
     </div>
   );
 }
---
+++ src/components/DonutChart.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Chart from "react-apexcharts"
+
+const DonutChart = () => {
+    var options = {}
+    var series = [44, 55, 41, 17, 15]
+    var labels = [44, 55, 41, 17, 15]
+
+    return (
+        <div className="donut">
+            <Chart options={options} series={series} type="donut" width="380" />
+        </div>
+    )
+}
+
+export default DonutChart
